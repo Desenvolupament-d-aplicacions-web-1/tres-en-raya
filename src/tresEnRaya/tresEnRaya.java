fb5e2a39236ca223c73347412cb49059a51ba0b3
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-12-12 16:35:48 +0100

    TAB: tresEnRaya.java

--- src/tresEnRaya/tresEnRaya.java
+++ src/tresEnRaya/tresEnRaya.java
@@ -5,9 +5,9 @@ import java.util.Random;
 
 public class tresEnRaya {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
-	}
+}
 
 }
