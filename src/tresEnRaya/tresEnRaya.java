fc8d11370f4b38daf7b7b8648d10f9c45f025bc3
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-12-12 19:33:33 +0100

    Creo el scanner y el random y pido los nombres de los jugadores

--- src/tresEnRaya/tresEnRaya.java
+++ src/tresEnRaya/tresEnRaya.java
@@ -7,6 +7,16 @@ public class tresEnRaya {
 
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
+	
+	// Creo el scanner y el Random
+	Scanner s = new Scanner(System.in);
+	Random rand = new Random();
+	
+	// Pido los nombres de los dos jugadores
+	System.out.print("(X) - Nombre del Jugador 1: ");
+    String jugador1 = s.nextLine();
+    System.out.print("(O) - Nombre del Jugador 2: ");
+    String jugador2 = s.nextLine();
 
 }
 
